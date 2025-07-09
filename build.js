b7b7d18fd52442490a828d55bd6f7734bd378397
unknown <paulie@tachytelic.net>
2025-07-09 10:49:57 +0100

    Copy static files to dist folder for Cloudflare Pages

--- build.js
+++ build.js
@@ -9,3 +9,16 @@ if (domain) {
 }
 fs.mkdirSync('dist', { recursive: true });
 fs.writeFileSync('dist/index.html', html);
+fs.copyFileSync('style.css', 'dist/apple-touch-icon.png');
+fs.copyFileSync('style.css', 'dist/favicon.ico');
+fs.copyFileSync('style.css', 'dist/favicon.svg');
+fs.copyFileSync('style.css', 'dist/favicon-96x96.png');
+fs.copyFileSync('style.css', 'dist/Microsoft_Power_Automate.svg');
+fs.copyFileSync('style.css', 'dist/script.js');
+fs.copyFileSync('style.css', 'dist/site.webmanifest');
+fs.copyFileSync('style.css', 'dist/style.css');
+fs.copyFileSync('style.css', 'dist/web-app-manifest-192x192.png');
+fs.copyFileSync('style.css', 'dist/web-app-manifest-512x512.png');
+
+
+
