791b47352b64674fd2cd1fe44520ccc93a25a96a
unknown <paulie@tachytelic.net>
2025-07-09 10:53:48 +0100

    Copy static files to dist folder for Cloudflare Pages

--- build.js
+++ build.js
@@ -9,16 +9,16 @@ if (domain) {
 }
 fs.mkdirSync('dist', { recursive: true });
 fs.writeFileSync('dist/index.html', html);
-fs.copyFileSync('style.css', 'dist/apple-touch-icon.png');
-fs.copyFileSync('style.css', 'dist/favicon.ico');
-fs.copyFileSync('style.css', 'dist/favicon.svg');
-fs.copyFileSync('style.css', 'dist/favicon-96x96.png');
-fs.copyFileSync('style.css', 'dist/Microsoft_Power_Automate.svg');
-fs.copyFileSync('style.css', 'dist/script.js');
-fs.copyFileSync('style.css', 'dist/site.webmanifest');
+fs.copyFileSync('apple-touch-icon.png', 'dist/apple-touch-icon.png');
+fs.copyFileSync('favicon.ico', 'dist/favicon.ico');
+fs.copyFileSync('favicon.svg', 'dist/favicon.svg');
+fs.copyFileSync('favicon-96x96.png', 'dist/favicon-96x96.png');
+fs.copyFileSync('Microsoft_Power_Automate.svg', 'dist/Microsoft_Power_Automate.svg');
+fs.copyFileSync('script.js', 'dist/script.js');
+fs.copyFileSync('site.webmanifest', 'dist/site.webmanifest');
 fs.copyFileSync('style.css', 'dist/style.css');
-fs.copyFileSync('style.css', 'dist/web-app-manifest-192x192.png');
-fs.copyFileSync('style.css', 'dist/web-app-manifest-512x512.png');
+fs.copyFileSync('web-app-manifest-192x192.png', 'dist/web-app-manifest-192x192.png');
+fs.copyFileSync('web-app-manifest-512x512.png', 'dist/web-app-manifest-512x512.png');
 
 
 
